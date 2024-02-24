ebb94b70b4a36eef04f8810e2550d7b521d01ebe
rakibhossainraju <rakibhossianraju@gmail.com>
2024-02-25 03:33:13 +0600

    done with the left side bar

--- src/pages/checkout-page/CheckoutPage.jsx
+++ src/pages/checkout-page/CheckoutPage.jsx
@@ -1,9 +1,11 @@
 import { NavbarComponent } from "../components/NavbarComponent.jsx";
+import { AddCustomerModal } from "../components/AddCustomerModal.jsx";
 
 export const HomePage = () => {
   return (
     <>
       <NavbarComponent />
+      <AddCustomerModal />
     </>
   );
 };
